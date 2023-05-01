41ed840b37646997f34bb0a31711a53330720f06
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-01 20:35:43 +0300

    Create Controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+  def splash
+    if current_user
+      redirect_to categories_url, alert: "You are already logged in as, #{current_user.name}"
+    else
+      render :splash
+    end
+  end
+
   def index
     @user = current_user
+  end
+
+  def redirect_to_categories
+    redirect_to categories_url
   end
 end
